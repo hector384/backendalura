dc53747b266b0f498b54dd13f37111c9d0379223
hector <hectorpelaez11@hotmail.com>
2024-07-09 10:49:57 -0500

    spring security

--- src/main/java/com/project/alura/infra/security/TokenService.java
+++ src/main/java/com/project/alura/infra/security/TokenService.java
@@ -5,15 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenService {
 
-    public String GenerarToken(){
-        try {
-            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
-            String token = JWT.create()
-                    .withIssuer("auth0")
-                    .sign(algorithm);
-        } catch (JWTCreationException exception){
-            // Invalid Signing configuration / Couldn't convert Claims.
-        }
-        return null;
-    }
+
 }
